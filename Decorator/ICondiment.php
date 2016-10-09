bcf829e2cbadde180adbf555cf466f872a79ceb8
liu <243498804@qq.com>
2016-10-10 00:03:04 +0800

    调味品接口

---
+++ Decorator/ICondiment.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 文件名：Condiment.php
+ * 编辑器：PhpStorm
+ * 作者: lf
+ * 日期: 2016/10/9 22:25
+ */
+
+namespace Decorator;
+interface ICondiment 
+{
+    function cost();//调味品价格
+    function name();//调味品名称
+}
